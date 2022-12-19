1077bcef00629c8c54235f1e73513832b79d9d69
EgorVjalkov <e.vjalkov@yandex.ru>
2022-12-19 22:46:56 +0300

    continue testing pandas

--- Classes.py
+++ Classes.py
@@ -2,11 +2,28 @@ import pandas as pd
 
 class CombinationOfMods:
     def __init__(self, path_to_test, path_to_price=''):
-        test = pd.read_csv(path_to_test, delimiter=';')
+        df = pd.read_csv(path_to_test, delimiter=';')
+        test = df.iloc[0:6] # срезы
+        # test[test['MOD']  True]
+        print(test['MOD'].fillna(False)) # замена НаН на false
         # pd.DataFrame(test, index=None, columns=1)
-        # est = list(pd.DataFrame.columns)
+        head = test.columns.to_list() # конвертация в list
+        # head = [i for i in list(test.head(0)) if 'Unnamed' not in i]
 
-        print(test['MOD'])
+        # self.c = test['ANSWER'].unique()
+        print(head)
+        self.mod = [i for i in test['MOD'].to_list() if i]
+
+        print(self.mod)
+        variants = {i: list(test[i]) for i in head}
+        # for i in variants:
+        #     print(type(i))
+        #     for e in i:
+        #         print(type(e))
+
+        # self.weak = 0
+
+        print(variants)
 
 
 
