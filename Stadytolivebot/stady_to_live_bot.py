2823c6f40e6dddd245eb86fd694e5b6af2d7280e
EgorVjalkov <e.vjalkov@yandex.ru>
2023-09-03 18:11:50 +0300

    начал работу с телегой, тормоз на уровне менюшек. Надоть читать больше

---
+++ Stadytolivebot/stady_to_live_bot.py
@@ -0,0 +1,54 @@
+import telebot
+from telebot import types
+
+
+DAYZ = ['1.09.2013', '2.09.2023']
+TOKEN = '6456866792:AAEXkQ5ZfrEj-WMRVJPr-StkOpJkUJWIMIA'
+ADMIN_ID = '831647128'
+
+bot = telebot.TeleBot(TOKEN)
+
+name = None
+username = None
+descriptions = {1: 'прыг_скок', 2: 'сядь на голову', 3: 'чигарики-чок-чигары'}
+variants = {1: ['1', '2'], 2: ['сясь'], 3: ['комарики', 'мухи', 'комары']}
+container = {1: '', 2: '', 3: ''}
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    global name, username
+    name = message.from_user.first_name
+    username = message.from_user.username
+    bot.send_message(message.chat.id, 'Привет. Это бот для заполнения таблицы. Выберите число для заполнения')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_message(message):
+    change_a_date(message)
+    container['date'] = message
+    for q in descriptions:
+        answer_a_question(message, q)
+
+
+def change_a_date(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for i in DAYZ:
+        b = types.KeyboardButton(i)
+        markup.add(b)
+    bot.send_message(message.chat.id, 'Выбери дату из списка', reply_markup=markup)
+    container['date'] = message.text
+
+
+def answer_a_question(message, q):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    answers = variants[q]
+    for a in answers:
+        b = types.KeyboardButton(a)
+        markup.add(b)
+    bot.send_message(message.chat.id, text=descriptions[q])
+    container[q] = message.text
+
+
+bot.polling()
+print(container)
