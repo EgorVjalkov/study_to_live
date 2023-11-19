42ff6c5edf66d4421170ce9a55b537c8afbe296e
EgorVjalkov <e.vjalkov@yandex.ru>
2023-11-19 22:12:09 +0300

    обавил гитигнор

--- path_maker.py
+++ path_maker.py
@@ -1,8 +1,14 @@
 import pathlib
 from pathlib import Path
+from os import mkdir
 
 
 path_to_project = pathlib.Path.cwd()
 
 month = 'nov23'
 path_to_vedomost = Path(path_to_project, 'months', month, f'{month}.xlsx')
+
+temp_dir = f'{month}_temp_db'
+path_to_temp_db = Path(path_to_project, 'months', month, temp_dir)
+if os.
+print(path_to_temp_db)
